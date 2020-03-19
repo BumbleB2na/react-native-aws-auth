8bfddd60e291108711eb1ff04c64cfd56a4ecbe4
b2 <brendan@mobilewebsmart.com>
2020-03-19 06:04:11 -0600

    UI and CRUD methods for GraphQL user data

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Button, Text, TextInput, View, ScrollView, Linking, Alert, Platform } from 'react-native';
+import { StyleSheet, Button, Text, TextInput, View, ScrollView, Linking, Alert, Platform, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 // make API calls to GraphQL service
 import Amplify from '@aws-amplify/core';
@@ -34,20 +35,8 @@ class App extends React.Component {
 	state = {
 		title: '',
 		url: 'https://',
-		hyperlinks: [
-			{
-				'title':'A Visited Url',
-				'url':'https://example.com',
-				'visited': true
-			},
-			{
-				'url':'https://example.com/#url-with-no-title'
-			},
-			{
-				'title':'A Bad Url',
-				'url':'htp://example.com'
-			},
-		]
+		hyperlinks: [],
+		owner: ''
 	};
 	resetInputState = () => {
 		this.setState({
@@ -57,6 +46,49 @@ class App extends React.Component {
 	};
 	onChangeText = (key, val) => {
 		this.setState({ [key]: val });
+	};
+	// get the current authenticated user to store "owner" details for GraphQL @auth and then, list the owned hyperlinks
+	componentDidMount = async () => {
+		try {
+		await Auth.currentAuthenticatedUser()    
+			.then(async (user) => {
+				// store "owner" details for GraphQL @auth
+				this.setState({
+					owner: user.attributes.sub
+				});
+
+				// Get a list of hyperlinks
+				const graphqldata = await API.graphql(graphqlOperation(ListHyperlinks));
+				console.log('graphqldata: ', graphqldata);
+
+				// Set state with a list of user's hyperlinks, sorted descending by createdOn ISO date
+				let hyperlinks = graphqldata.data.listHyperlinks.items
+					.sort(function(a, b) {
+						return (a.createdOn > b.createdOn) ? -1 : ((a.createdOn < b.createdOn) ? 1 : 0);
+					});
+				this.setState({ hyperlinks: hyperlinks });
+			});
+		}
+		catch(error) {
+			console.log('error: ', error);
+		}
+	};
+	onPressDeleteHyperlink = async (index) => {
+		let hyperlinks = this.state.hyperlinks;
+		const hyperlinkId = hyperlinks[index].id;
+
+		// Update hyperlinks in state to reflect the change
+		hyperlinks.splice(index, 1);
+		this.setState({
+			hyperlinks: hyperlinks
+		})
+
+		const deletedHyperlink = {
+			id: hyperlinkId, 
+			// owner: this.state.owner
+		}
+		await API.graphql(graphqlOperation(DeleteHyperlink, deletedHyperlink));
+		console.log('Deleted hyperlink from database');
 	};
 	onPressSaveForLaterButton = () => {
 		const isUrlEmpty = this.state.url.trim() === '';
@@ -81,8 +113,8 @@ class App extends React.Component {
 	setFocusToHyperlinkInput = () => {
 		this.hyperlinkInput.focus();
 	};
-	openHyperlink = (hyperlinkObj, index) => {
-		this.flagHyperlinkAsVisited(index);
+	openHyperlink = async (hyperlinkObj, index) => {
+		await this.flagHyperlinkAsVisited(index);
 
 		const url = hyperlinkObj.url;
 		Linking.canOpenURL(url)
@@ -97,37 +129,66 @@ class App extends React.Component {
 				this.showAlertMessage('Whoops','Cannot go to this saved link');
 			});
 	};
-	flagHyperlinkAsVisited = (index) => {
+	flagHyperlinkAsVisited = async (index) => {
 		let hyperlinks = this.state.hyperlinks;
 		hyperlinks[index].visited = true;
 		this.setState({
 			hyperlinks: hyperlinks
 		})
+
+		try {
+			const updatedHyperlink = {
+				id: hyperlinks[index].id, 
+				visited: hyperlinks[index].visited, 
+				owner: this.state.owner
+			}
+			await API.graphql(graphqlOperation(UpdateHyperlink, updatedHyperlink));
+			console.log('Updated hyperlink in database');
+		}
+		catch(error) {
+			this.showAlertMessage('Whoops','Failed to flag this link as visited for good');
+			console.log('error: ', error);
+		}
 	};
 	isUrlValid = (url) => {
 		var isValid = url.length > 9 && (url.substr(0, 7) === 'http://' || url.substr(0, 8) === 'https://');
 		return isValid;
 	};
-	saveHyperlink = () => {
+	saveHyperlink = async () => {
 		let newHyperlink = this.createHyperlinkFromState();
 		let hyperlinks = this.state.hyperlinks;
 		hyperlinks.unshift(newHyperlink);
 		this.setState({
 			hyperlinks: hyperlinks
-		})
+		});
+
+		try {
+			const hyperlink = await API.graphql(graphqlOperation(AddHyperlink, newHyperlink));
+			console.log('Created hyperlink in database');
+
+			// update hyperlink in state with its id so that if updated later the change can be persisted to database
+			this.state.hyperlinks[0].id = hyperlink.data.createHyperlink.id;
+		}
+		catch(error) {
+			this.showAlertMessage('Whoops','Failed to save this link for good');
+			console.log('error: ', error);
+		}
 	};
 	createHyperlinkFromState = () => {		
 		let hyperlink = {
-			url: this.state.url.trim()
+			url: this.state.url.trim(),
+			visited: false,
+			createdOn: (new Date()).toISOString(),
+			owner: this.state.owner
 		}
 		if(this.state.title)
-			hyperlink.title = this.state.title.trim();
+			hyperlink.title = this.state.title;
 		return hyperlink;
 	};
 	render() {
 		return (
 			<View style={styles.wrapper}>
-				<View style={styles.footer}>
+				<View style={styles.header}>
 					<Text style={styles.title}>Save For Later</Text>
 					<Text style={styles.subtitle}>What do you want to read, watch or listen to?</Text>
 					<TextInput
@@ -156,19 +217,27 @@ class App extends React.Component {
 						onPress={this.onPressSaveForLaterButton}
 						title="Save For Later"
 						accessibilityLabel="Save Your Hyperlink For Later"
-						// color="#eeaa55"
+						color="#ff9900"
+						padding={44}
 					/>
 				</View>
 				<ScrollView style={styles.list}>
-					<Text style={styles.title}>Your Saves For Later</Text>
+					<Text style={styles.title}>Your Saved Links</Text>
 					{this.state.hyperlinks.map((hyperlink, index) => (
-						<View key={index}>
+						<View key={index} style={styles.hyperlinkRow}>
 							<Text
 								style={[styles.hyperlink, hyperlink.visited ? styles.hyperlinkVisited : undefined]}
 								onPress={() => this.openHyperlink(hyperlink, index)}
 							>
 								{ hyperlink.title || hyperlink.url }
 							</Text>
+							<TouchableOpacity style={styles.hyperlinkDelete} onPressOut={() => this.onPressDeleteHyperlink(index)}>
+								<MaterialIcons
+									name="delete-forever"
+									size={24}
+									color="#2d2d2d"
+								/>
+							</TouchableOpacity>
 						</View>
 					))}
 				</ScrollView>
@@ -241,14 +310,20 @@ mutation ($title: String $url: String! $visited: Boolean! $createdOn: String! $o
 
 const styles = StyleSheet.create({
 	wrapper: {
-		flex: 1
+		flex: 1,
+		borderTopWidth: 1,
+		borderTopColor: '#999'
 	},
 	list: {
 		backgroundColor: '#ccc',
 		paddingHorizontal: 10,
 		paddingVertical: 20
 	},
+	hyperlinkRow: {
+		flexDirection: 'row'
+	},
 	hyperlink: {
+		flex: 1,
 		fontSize: 20,
 		color: 'blue',
 		marginVertical: 10,
@@ -260,13 +335,16 @@ const styles = StyleSheet.create({
 		color: 'purple',
 		textDecorationLine: 'line-through'
 	},
-	footer: {
+	hyperlinkDelete: {
+		marginTop: 10
+	},
+	header: {
 		borderBottomColor: '#999',
 		borderBottomWidth: 1,
 		backgroundColor: '#fff',
 		paddingHorizontal: 10,
 		paddingVertical: 20,
-		paddingTop: 40
+		paddingTop: 20
 	},
 	title: {
 		fontSize: 20,
